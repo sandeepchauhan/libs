a792eafdeb3ddbe4f583eb32833c4c0b0c7ece1e
Sandeep Chauhan <sanch@microsoft.com>
2015-06-30 14:04:53 +0530

    Commit

---
+++ Utils/Utils/GUIDUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.Libs.Utils
+{
+    public class GUIDUtils
+    {
+        public static Guid GetNewGUID()
+        {
+            return new Guid();
+        }
+    }
+}
